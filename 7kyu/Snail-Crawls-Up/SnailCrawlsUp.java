4471c0c443924de33f2ad195f84f5c2e4f5dea6b
Ana Saavedra <asaavedrayandun@cifpfbmoll.eu>
2024-03-03 10:09:46 +0100

    Codewars Kyu 7

---
+++ 7kyu/Snail-Crawls-Up/SnailCrawlsUp.java
@@ -0,0 +1,31 @@
+// The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+// Your function takes three arguments:
+
+// The height of the column (meters)
+// The distance that the snail crawls during the day (meters)
+// The distance that the snail slides down during the night (meters)
+// Calculate number of day when the snail will reach the top of the column.
+
+public class SnailCrawlsUp {
+    public static int snail(int column, int day, int night) {
+      
+      int distance = 0;
+      int totalDays = 0;
+      
+      while(distance < column){
+        totalDays ++; 
+        distance += day; 
+        if (distance >= column){
+        break; 
+        }
+        distance -= night; 
+      }
+
+      // distancia total: column 
+      // distancia recorrida en un día: day - nigth 
+      // número total de días: column / (day - nigth) 
+      return totalDays; 
+    } 
+  
+}
